995d0f74267d3a3bc6884bd93b9a41de55cb2f5f
Nguyen Trong Nhat <nguyentrongnhat240700@gmail.com>
2022-11-26 18:00:40 +0700

    hoàn thành phần cài đặt + đổi màu giao diện, giao diện sáng - tối

--- js/skin-switcher.js
+++ js/skin-switcher.js
@@ -1,21 +1,9 @@
-// $(document).ready(function () {
-$(window).on('load', function () {
+$(document).ready(function () {
     /*--------------------------------
     HIỆN - ẨN KHUNG CÀI ĐẶT
     --------------------------------*/
     $('.style-switcher-toggler').click(function () {
         $('.skin-switcher').toggleClass('open-switcher').siblings().removeClass('open-switcher');
-    });
-
-    /*-------------------------
-    ẨN KHUNG CÀI ĐẶT KHI CUỘN
-    -------------------------*/
-    $(window).scroll(function () {
-        if ($('.skin-switcher').hasClass('open-switcher')) {
-            setTimeout(function () {
-                $('.skin-switcher').removeClass('open-switcher');
-            }, 300);
-        }
     });
 
     /*--------------------
@@ -42,8 +30,32 @@ $(window).on('load', function () {
     function changeSkinColor(skinColor) {
         $(':root').css('--skin-color-used', 'var(--' + skinColor + ')');
     }
+});
 
-    /*------------------------------
-    THAY ĐỔI GIAO DIỆN NGÀY - ĐÊM
-    -------------------------------*/
+/*-------------------------
+ẨN KHUNG CÀI ĐẶT KHI CUỘN
+-------------------------*/
+$(window).scroll(function () {
+    if ($('.skin-switcher').hasClass('open-switcher')) {
+        setTimeout(function () {
+            $('.skin-switcher').removeClass('open-switcher');
+        }, 300);
+    }
+});
+
+/*------------------------------
+THAY ĐỔI GIAO DIỆN NGÀY - ĐÊM
+-------------------------------*/
+$(window).on('load', function () {
+    $('.day-night').click(function () {
+        // thay đổi icon ngày - đêm
+        $(this).children('i').toggleClass('fa-sun').siblings().removeClass('fa-sun');
+        $(this).children('i').toggleClass('fa-moon').siblings().removeClass('fa-moon');
+        $('body').toggleClass('dark').siblings().removeClass('dark');
+    });
+
+    // thay đổi ngày - đêm sau khi load
+    if ($('body').hasClass('dark')) {
+        $('.day-night').children('i').toggleClass('fa-sun').siblings().removeClass('fa-sun');
+    }
 });
