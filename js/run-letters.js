3e94e5a4cd63d16e3ad354961c99a84f78bc444b
Nguyen Trong Nhat <nguyentrongnhat240700@gmail.com>
2022-11-27 16:55:41 +0700

    cập nhật nhẹ

--- js/run-letters.js
+++ js/run-letters.js
@@ -2,7 +2,7 @@
 CHẠY CHỮ Ở TRANG GIỚI THIỆU - HOME
 -----------------------------------*/
 var typed = new Typed('.typing', {
-    strings: ['', 'lập trình viên website!', 'sinh viên FPT Polytechnic!', 'web developer intern >◡◔'],
+    strings: ['..', 'lập trình viên website!', 'sinh viên FPT Polytechnic!', 'web developer intern >◡◔', '..'],
     typeSpeed: 70,
     backSpeed: 10,
     loop: true,
